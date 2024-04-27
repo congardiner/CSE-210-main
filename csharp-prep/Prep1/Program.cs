2dc11b3cfb03eeb454806d3633436d940aebb3ba
congardiner <18cgardiner1@gmail.com>
2024-04-26 21:02:28 -0600

    Adjustments made to the folders, creation of subtypes, and prep #1 has been worked out, with the notes and studying that I've done made within.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name? ");
+        string firstcg = Console.ReadLine();
+        // Console.WriteLine($"Your first name is {firstcg}.");
+
+        Console.WriteLine("What is your last name? ");
+        string lastcg = Console.ReadLine();
+        // Console.WriteLine($"Your last name is {lastcg}.");
+
+        Console.WriteLine($"Your name is {lastcg}, {firstcg} {lastcg}");
+
+
+
+
+
     }
 }
