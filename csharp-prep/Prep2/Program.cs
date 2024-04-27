7008bd48937a0250167bf073793fad7db81286f4
congardiner <18cgardiner1@gmail.com>
2024-04-27 12:36:34 -0600

    Curly brackets were not put in my closing for my if loop, now it is closed and working correctly.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,39 +8,51 @@ class Program
         string usercg = Console.ReadLine();
         Console.WriteLine("");
         
+        string lettercg; // Declare the variable lettercg here
+
         if (double.TryParse(usercg, out double gradepercentcg))
+        {
             if (gradepercentcg >= 90)
             {
+                lettercg = "A";
                 Console.WriteLine($"Your percentage of {gradepercentcg} Grade Percentage was more than 90%, so you got an A!");
             }
 
             else if (gradepercentcg >= 80)
             {
+                lettercg = "B";
                 Console.WriteLine($"Your percentage of {gradepercentcg} Grade Percentage was more than 80%, so you got an B!");
             }
 
             else if (gradepercentcg >= 70)
             {
+                lettercg = "C";
                 Console.WriteLine($"Your percentage of {gradepercentcg} Grade Percentage was more than 70%, so you got an C!");
             }
 
             else if (gradepercentcg >= 60)
             {
-                Console.WriteLine($"Well you did it, just barely getting a percentage of {gradepercentcg}, with a D! This isn't enough to pass the course, but you will still receieve credit.");
+                lettercg = "D";
+                Console.WriteLine($"Well you did it, just barely getting a percentage of {gradepercentcg}, with a D! This isn't enough to pass the course, but you will still receive credit.");
             }
 
             else
             {
+                lettercg = "F";
                 Console.WriteLine($"Unfortunately with your grade percentage of {gradepercentcg}, this is not a passing grade. Retake or remedial is now necessary!");
             }
 
+            Console.WriteLine($"The Letter Earned for this course is the following: {lettercg} all due to the work that you put in!");
+        }
+        
         else
         {
             Console.WriteLine("Try again with your input, as this appears to not be a valid number, or in the correct notation for my basic program.");
         }
-        // if (gradepercentcg >= "70")
     }
+    
 }
+
 
 
 //practice from my notes to review and learn
