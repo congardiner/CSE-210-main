15ef16240ea06e52dbfce4efa8ba022fa0a9fceb
congardiner <18cgardiner1@gmail.com>
2024-04-29 09:11:01 -0600

    Update changes to my practice folder, to store class notes and other infastructure control for learning csharp materials.

---
+++ sandbox/Sandbox/practice/Prep1/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int x=0;
+        if (x == 0);
+
+        
+
+
+
+    }
+}
