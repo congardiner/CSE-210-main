f3e176c13abfbb5335b15f374b9fb93c79ec4d2b
congardiner <18cgardiner1@gmail.com>
2024-04-29 18:55:48 -0600

    Released changes to sandbox environment code.

--- sandbox/Sandbox/practice/Prep1/Program.cs
+++ sandbox/Sandbox/practice/Prep1/Program.cs
@@ -1,15 +1,35 @@
 using System;
-using System.Net.WebSockets;
 using System.Security.Cryptography.X509Certificates;
 
 class Program
+
 {
     static void Main(string[] args)
     {
         int x=0;
-        if (x == 0);
 
-        
+        if (x==0)
+        {
+            Console.WriteLine("testing code.");
+        }
+
+        while (x<3)
+        {
+            Console.WriteLine("H.W.");
+            x += 1;
+            // x -= 1;
+            // x *= 1;
+            // x %= 1;
+            // x /= 1;
+        }
+
+        // for x in range(9): print("Hello World") this is how it is done within python
+
+        for (int x=0; x<9; x++)
+        {
+            Console.WriteLine("Howdy Sandbox World!");
+        }
+
 
 
 
