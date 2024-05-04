effdf9b55ebd12dfa29402ca7233c0ff65ad7972
congardiner <18cgardiner1@gmail.com>
2024-05-03 21:29:57 -0600

    My number notation game is not routing correctly, I could insert some functions to encapsulate it better so that it will indeed run; will troubleshoot some more to isolate the issue within prep #4.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -6,21 +6,27 @@ class Program
     static void Main(string[] args)
     {
 
-        List<string> numbers = new List<string>();
+        List<int> numbers = new List<int>();
         
-        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
-
-
-        string input;
-        while ((input = Console.ReadLine()) != "0")
-
+        int userNumber = -1;
+        while (userNumber !=0)
         {
-            numbers.Add(input);
+
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+
+            string userResponse = Console.ReadLine();
+            userNumber = int.Parse(userResponse);
+
+            if (userNumber !=0)     
+            {
+                numbers.Add(userNumber);
+            }
         }
 
         Console.WriteLine("Enter a number: ");
 
-        foreach (string number in numbers)
+        foreach (int number in numbers)
         {
             Console.WriteLine(number);
         }
@@ -30,21 +36,20 @@ class Program
         {
             sum+= number;
         }
-        
-        
 
+         
         Console.WriteLine($"The Sum is: {sum}");
 
         float average = ((float)sum) / numbers.Count;
 
-        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The average is {average}");
 
         int max = numbers[0];
         foreach (int number in numbers)
         {
             if (number > max)
             {
-                max = number
+                max = number;
             }
         }
 
