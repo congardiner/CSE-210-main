145963997f3221303679822a53b8308310443fc1
congardiner <18cgardiner1@gmail.com>
2024-05-13 08:58:50 -0600

    Updates made to abstraction, notation made within for this week's program, and journal outline for the object orientation.

--- sandbox/abstraction.cs
+++ sandbox/abstraction.cs
@@ -8,11 +8,14 @@ class mainMenu {
     public void Display()
     {
         Console.WriteLine("The Ultimate Journaling System. Does it have a name? Well no, but you do and that's what matters. This is the all in one hub, documenting and storing your memories for the future!");
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine("");
+        Console.WriteLine("This is the Main Menu:");
+        Console.WriteLine("Access File (1)");
+        Console.WriteLine("New Prompt (2)")
+        Console.WriteLine("Save File (3)");
+        Console.WriteLine("Quit Program (4)");
     }
+
+    
 
     public void journalPrompt()
     {
