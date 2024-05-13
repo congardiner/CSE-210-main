83b339625895e4ab7f39a7bac970ed6274cf1cf4
congardiner <18cgardiner1@gmail.com>
2024-05-13 09:08:03 -0600

    Updates made to the Journal.

--- sandbox/abstraction.cs
+++ sandbox/abstraction.cs
@@ -2,6 +2,8 @@ using System;
 using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 
+// Some creative notes that I could include, is inserting a few parameters into the prompt generator, so that the prompts don't overlap or repeat until they've been experienced.
+// I could also make sure that the user has the option to choose a different prompt if they want to. 
 class mainMenu {
     static void Main(string[] args)
 
@@ -9,13 +11,13 @@ class mainMenu {
     {
         Console.WriteLine("The Ultimate Journaling System. Does it have a name? Well no, but you do and that's what matters. This is the all in one hub, documenting and storing your memories for the future!");
         Console.WriteLine("This is the Main Menu:");
-        Console.WriteLine("Access File (1)");
-        Console.WriteLine("New Prompt (2)")
-        Console.WriteLine("Save File (3)");
-        Console.WriteLine("Quit Program (4)");
+        Console.WriteLine("Access File (type 1)");
+        Console.WriteLine("New Prompt (type 2)")
+        Console.WriteLine("Save File (type 3)");
+        Console.WriteLine("Quit Program (type 4)");
     }
 
-    
+
 
     public void journalPrompt()
     {
