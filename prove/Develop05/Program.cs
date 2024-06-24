3474a1e125bc6f6070fbc8534d991ec26510d296
congardiner <18cgardiner1@gmail.com>
2024-06-24 08:58:06 -0600

    Updates made to the base classes, using the structure that I made within my class diagrams.

--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop05 World!");
+        bool exit = false;
+
+        while (!exit)
+        {
+            Console.Clear();
+            Console.WriteLine("Making Progress Starts Here!");
+            Console.WriteLine("The Eternal Quest: Its Eternal");
+            Console.WriteLine("1. Simple Goal");
+            Console.WriteLine("2. Eternal Goal");
+            Console.WriteLine("3. Checklist Goal");
+            Console.WriteLine("4. Save Goal");
+            Console.WriteLine("5. Load Goals");
+            Console.WriteLine("6. Exit Program");
+
+            Console.WriteLine("Enter Your Choice");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "1":
+                    
+                    break;
+
+                case "2":
+                    
+                    break;
+
+                case "3":
+                    
+                    
+                    break;
+                case "4":
+
+                    break;
+                
+                case "5":
+
+                    break;
+                    
+                case "6":
+                    exit = true;
+                    break;
+
+                default:
+                    Console.WriteLine("This is not a valid response. Please enter either 1, 2, or 3 to use the application. Enter 4 to exit the program.");
+                    break;
+            }
+
+            if (!exit)
+            {
+                Console.WriteLine("Press any key to enter the menu, or startup the program please!");
+                Console.ReadKey();
+            }
+        }
     }
 }
--- prove/Develop05/goal.cs
+++ prove/Develop05/goal.cs
@@ -1,8 +1,33 @@
 using System;
+using System.Net;
 
 public class Goal
 {
-    
+    private int points;
+
+    public virtual string AddActivity()
+    {
+
+
+
+
+    }
+
+    public virtual string ShowScore()
+    {
+        // add a constructor to get base score, ++ score, and add other scores //
+
+
+    }
+
+    public virtual string ShowList()
+    {
+        // show the list of active goals that have been outputted. //
+
+
+
+
+    }
 
 
 
