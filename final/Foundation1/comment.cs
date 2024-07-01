d55213e5fab1f955cd39d318e756906345491192
congardiner <18cgardiner1@gmail.com>
2024-07-01 09:51:35 -0600

    Commited changes to class creation(s), added variables, get methods for my classes, and constructors for them as well to actuate my class variables.

---
+++ final/Foundation1/comment.cs
@@ -0,0 +1,21 @@
+using System;
+
+
+public class Comment
+{
+
+    public string _personName;
+    public string _personComment;
+
+    // constructor to activate my variables.
+
+    // will need a method for creating a person, and storing comment with them //
+
+    // public CreatePerson()
+    // public CreateComment(person)
+
+
+
+
+
+}
---
+++ final/Foundation1/video.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+
+public class Video 
+{
+    static string _title;
+    static string _author;
+    static int _length;
+    // measured in seconds, just a note to remind me later //
+
+    // constructor for activating my variables and then displaying them with a get and set. //
+
+    // method for GetVideoDescription()
+
+
+
+
+
+}
