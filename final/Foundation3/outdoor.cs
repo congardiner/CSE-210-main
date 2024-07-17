35bc87bc55b36401c8fe348a2d62ffe44e43e303
congardiner <18cgardiner1@gmail.com>
2024-07-17 09:23:00 -0600

    Test-cased the builtin .net DateTime method for displaying the time within the class.

--- final/Foundation3/outdoor.cs
+++ final/Foundation3/outdoor.cs
@@ -6,8 +6,16 @@ public class Outdoor : Event
     private string _weatherForeCast;
     private string _weatherTemp;
 
-    public Outdoor(string weatherForeCast, string weatherTemp, string eventTitle, string eventType, string eventDescription, string time, string eventAddress, string eventDate) : base(eventTitle, eventDescription, time, eventAddress, eventDate)
-    {
+    public Outdoor(string weatherForeCast, string weatherTemp, string eventTitle, string eventType, string eventDescription, string time, string eventAddress, string eventDate)
+        {
+        // edit note: need to actuate the rest of my constructors from the parent class, as this is where I left off //
+        _eventTitle = eventTitle;
+        _eventType = eventType;
+        _eventDescription = eventDescription;
+        // need to adjust the time constraints as builtin by .net //
+        _time = DateTime;
+        _eventAddress = eventAddress;
+        _eventDate = eventDate;
         _weatherForeCast = weatherForeCast;
         _weatherTemp = weatherTemp;
 
@@ -15,11 +23,10 @@ public class Outdoor : Event
 
     // need to just attach one of my methods from the parent class, and then display it here in conjunction with my GetDetails Method. //
 
-    public string GetDetails()
+    public override string GetFullDetails()
     {
-        Console.WriteLine($"The weather forecast for the Event is {_weatherForeCast} & temperature being {_weatherTemp}");
+        return base.GetFullDetails() + $"The weather forecast for the Event is {_weatherForeCast} & temperature being {_weatherTemp}";
     }
-
 
 
 }
